5372bd8854d598496968d09522a9f5ff47234b27
sahib573 <gurdeepsingh74108@gmail.com>
2023-09-08 10:18:30 +0530

    Gourps color scheme changed

--- frontend/src/components/GroupCard.tsx
+++ frontend/src/components/GroupCard.tsx
@@ -1,65 +1,74 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 interface GroupCardProps {
-    groupName: string;
-    totalExpenses: number;
-    members: string[];
-    amountsOwed: number[];
+  groupName: string;
+  totalExpenses: number;
+  members: string[];
+  amountsOwed: number[];
 }
 
 const GroupCard: React.FC<GroupCardProps> = ({
-    groupName,
-    totalExpenses,
-    members,
-    amountsOwed,
+  groupName,
+  totalExpenses,
+  members,
+  amountsOwed,
 }) => {
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    const toggleCard = () => {
-        setIsOpen(!isOpen);
-    };
+  const toggleCard = () => {
+    setIsOpen(!isOpen);
+  };
 
-    return (
-        <div className="max-w-sm rounded overflow-hidden border-b border-[#514644] shadow-lg bg-[#efc8b1]">
-            <div className="px-6 py-4">
-                <div className="flex justify-between">
-                    <div
-                        className={`font-semibold text-xl mb-2 ${isOpen ? 'text-blue-600' : 'text-black'
-                            }`}
-                    >
-                        {groupName}
-                    </div>
-                    <button
-                        onClick={toggleCard}
-                        className="float-right rounded-full bg-[#514644] text-white p-2 hover:bg-yellow transition-colors duration-300 ease-in-out"
-                    >
-                        {isOpen ? 'Show Less ↑' : 'Show More ↓'}
-                    </button>
-                </div>
-                <div
-                    className={`transition-max-height ease-out duration-300 ${isOpen ? 'max-h-96' : 'max-h-0 overflow-hidden'
-                        }`}
-                >
-                    <p className="text-lg text-gray-800">
-                        Total Expenses: ${totalExpenses.toFixed(2)}
-                    </p>
-                    <ul className="mt-3">
-                        {members.map((member, index) => {
-                            const amountOwed = amountsOwed[index];
-                            const isOwing = amountOwed > 0;
-
-                            return (
-                                <li key={index} className="text-lg">
-                                    {member}<p className={`text-base ${isOwing ? 'text-green' : 'text-red'}`}>{isOwing ? 'Owes You' : 'You Owe'}: ${Math.abs(amountOwed).toFixed(2)}</p>
-
-                                </li>
-                            );
-                        })}
-                    </ul>
-                </div>
-            </div>
+  return (
+    <div className="max-w-sm rounded hover:shadow-md overflow-hidden hover:scale-105 transition duration-500 ease-in-out hover:shadow-black-100 bg-[#5B39CF]">
+      <div className="px-6 py-4">
+        <div className="flex justify-between">
+          <div
+            className={`font-semibold text-xl mb-2 ${
+              isOpen ? "text-white" : "text-white"
+            }`}
+          >
+            {groupName}
+          </div>
+          <button
+            onClick={toggleCard}
+            className="float-right rounded-full bg-[#7A3DDF] text-white p-2 hover:bg-yellow transition-colors duration-300 ease-in-out"
+          >
+            {isOpen ? "Show Less ↑" : "Show More ↓"}
+          </button>
         </div>
-    );
+        <div
+          className={`transition-max-height ease-out duration-300 ${
+            isOpen ? "max-h-96" : "max-h-0 overflow-hidden"
+          }`}
+        >
+          <p className="text-lg text-white">
+            Total Expenses: ${totalExpenses.toFixed(2)}
+          </p>
+          <ul className="mt-3">
+            {members.map((member, index) => {
+              const amountOwed = amountsOwed[index];
+              const isOwing = amountOwed > 0;
+
+              return (
+                <li key={index} className="text-white text-lg">
+                  {member}
+                  <p
+                    className={`text-base ${
+                      isOwing ? "text-[#00F609]" : "text-[#FC5E3A]"
+                    }`}
+                  >
+                    {isOwing ? "Owes You" : "You Owe"}: $
+                    {Math.abs(amountOwed).toFixed(2)}
+                  </p>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default GroupCard;
